0f4ed96ad42bdac973927a79425f658751079ec6
ShlokMukkawar <shlok.m@econceptual.com>
2024-09-21 15:08:47 +0530

    handle Chart.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName:'Shlok'}; 
   return (
-    <div>Home</div>
+    <section className='home'>
+    <div className='home-content'>
+        <div className='home-header'>
+        <HeaderBox
+            type="greeting" 
+            title="Welcome"
+            user= {loggedIn?.firstName || "guest"}
+            subtext="Acess and Manage account smoothly." 
+        />
+        <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+        />
+        </div>
+    </div>
+    </section>
   )
 }
 
